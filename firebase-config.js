fbee6f6947cbbc2b4d23982ba2f5b49ee9708313
MayorFalomo <mayorfalomo@gmail.com>
2023-05-30 09:48:49 +0100

    edited netlify.toml

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import {getStorage} from "firebase/storage"
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+   apiKey: "AIzaSyDIVtpsPh4Ual_ZcK83sa9UcV_yDBRPXoU",
   authDomain: "twitter-1fecf.firebaseapp.com",
   projectId: "twitter-1fecf",
   storageBucket: "twitter-1fecf.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  messagingSenderId: "918026491792",
+  appId: "1:918026491792:web:4766d5c9f6cf59451e4515",
   measurementId: "G-VYQTZ5WEFV"
 };
 
