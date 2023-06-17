8c9d61c5f15d92c01d8d39080b26c839d11a5cd3
MayorFalomo <mayorfalomo@gmail.com>
2023-06-17 08:21:45 +0100

    protected keys

--- firebase-config.js
+++ firebase-config.js
@@ -7,9 +7,9 @@ const firebaseConfig = {
    apiKey: process.env.NEXT_PUBLIC_API_KEY ,
   authDomain: "twitter-1fecf.firebaseapp.com",
   projectId: "twitter-1fecf",
-  storageBucket: "twitter-1fecf.appspot.com",
-  messagingSenderId: "918026491792",
-  appId: "1:918026491792:web:4766d5c9f6cf59451e4515",
+  storageBucket:  "twitter-1fecf.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_ID ,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
   measurementId: "G-VYQTZ5WEFV"
 };
 
